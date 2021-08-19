48820ef9997c698f26d23ae3a90216442c181f2d
pyama <pyama@pepabo.com>
2021-08-19 14:34:02 +0900

    use graphql

--- run.rb
+++ run.rb
@@ -37,110 +37,116 @@ client = Octokit::Client.new(
 )
 
 config["orgs"].each do |o|
-  repos = begin
-            client.organization(o)
-            client.organization_repositories(o)
-          rescue
-            client.repositories(o)
-          end
-  repos.map{|m| m[:name] }.each do |r|
-    logger.info "start #{o}/#{r}"
-    Query = SWAPI::Client.parse <<~GRAPHQL
-    query {
-              repository(owner: "#{o}", name: "#{r}") {
-                  packages(first:100){
-                    nodes {
-                        id
-                        name
-                        packageType
-                        versions(first:100) {
-                            nodes {
-                                id
-                                version
-                                files(first: 10) {
-                                    nodes {
-                                        name
-                                        updatedAt
-                                    }
-                                }
-                            }
-                        }
-                    }
-                  }
-              }
+  Object.send(:remove_const, :Query) if Object.constants.include?(:Query)
+
+  type = begin
+           client.organization(o)
+           "organization"
+         rescue 
+           "user"
+         end
+
+  Query = SWAPI::Client.parse <<~GRAPHQL
+{
+  #{type}(login: "#{o}") {
+    packages(packageType: DOCKER, first: 100) {
+      edges {
+        node {
+          name
+          id
+          repository {
+            name
           }
-    GRAPHQL
+          versions(first: 1, orderBy: {field: CREATED_AT, direction: DESC}) {
+            nodes {
+              id
+              version
+            }
+          }
+        }
+      }
+    }
+  }
+}
+  GRAPHQL
 
-    response = SWAPI::Client.query(Query)
-    result = {}
-    if data = response.data
+  response = SWAPI::Client.query(Query)
+  result = {}
+  if data = response.data
+    data.to_h[type]["packages"]["edges"].each do |i|
+      r = i["node"]["repository"]["name"]
+      next if i["node"]["versions"]["nodes"].empty?
       begin
-        data.to_h["repository"]["packages"]["nodes"].select {|n| n["packageType"] == "DOCKER" }.each do |i|
-          image_name = "#{config["registory_domain"]}/#{o}/#{r}/#{i["name"]}:#{i["versions"]["nodes"].first["version"]}"
+        imn  = i["node"]["name"]
+        v = i["node"]["versions"]["nodes"].first["version"]
+        image_name = "#{config["registory_domain"]}/#{o}/#{r}/#{imn}:#{v}"
 
-          logger.info "check image name #{image_name}"
+        logger.info "check image name #{image_name}"
 
-          next if config["ignore_images"].find {|i| image_name =~ /#{i}/ }
+        next if config["ignore_images"].find {|i| image_name =~ /#{i}/ }
 
-          image = Docker::Image.create('fromImage' => image_name)
-          vols = []
-          vols << "#{Dir.pwd}/cache:/root/.cache"
-          vols << "/var/run/docker.sock:/var/run/docker.sock"
-          container = ::Docker::Container.create({
-            'Image' => trivy.id,
-            'HostConfig' => {
-              'Binds' => vols,
-            },
-            'Cmd' => [
-              "--ignore-unfixed",
-              "-s",
-              "HIGH,CRITICAL",
-              "--format",
-              "template",
-              "--template",
-             "@contrib/html.tpl",
-               "--exit-code",
-              "1",
-              image_name
-            ]
-          })
+        image = Docker::Image.create('fromImage' => image_name)
+        vols = []
+        vols << "#{Dir.pwd}/cache:/root/.cache"
+        vols << "/var/run/docker.sock:/var/run/docker.sock"
+        container = ::Docker::Container.create({
+          'Image' => trivy.id,
+          'HostConfig' => {
+            'Binds' => vols,
+          },
+          'Cmd' => [
+            "--ignore-unfixed",
+            "-s",
+            "HIGH,CRITICAL",
+            "--format",
+            "template",
+            "--template",
+            "@contrib/html.tpl",
+            "--exit-code",
+            "1",
+            image_name
+          ]
+        })
 
-          container.tap(&:start).attach do |stream, chunk|
-            logger.debug "#{stream} #{chunk}"
-            result[image_name] ||= {}
-            result[image_name][stream] ||= []
-            result[image_name][stream] << chunk
-          end
-
-          result[image_name][:status_code] = container.wait["StatusCode"]
-          container.remove(:force => true)
-          logger.info "check result #{o}/#{r} exit:#{result[image_name][:status_code]}"
+        container.tap(&:start).attach do |stream, chunk|
+          result[image_name] ||= {}
+          result[image_name][stream] ||= []
+          result[image_name][stream] << chunk
         end
 
-        next if result.empty? || result.all? {|_,v| v[:status_code] == 0 }
-
-        t = ["# These image has vulnability."]
-        result.each do |k,v|
-          t << v[:stdout]
-        end
-
-        issue_txt = t.join("\n")
-          .gsub(/<head>.+?<\/head>/m, '')
-          .gsub(/<\/?body>/m, '')
-          .gsub(/<\/?html>/m, '')
-          .gsub(/^\d{4}-\d{2}-\d{2}T\d{2}.+$/, '')
-          .gsub(/<!DOCTYPE html>/, '')
-          .gsub(/^\s*$/, '')
-          .gsub(/^\n/, '')
-          .gsub(/^    /m, '')
-
-        logger.info "create issue #{o}/#{r}"
-        client.create_issue("#{o}/#{r}", "#{Date.today.strftime("%Y/%m/%d")} Found vulnerabilities in docker image", issue_txt)
-      rescue => e
-        logger.error "#{o}/#{r} happend error #{e}"
+        result[image_name][:status_code] = container.wait["StatusCode"]
+        container.remove(:force => true)
+        logger.info "check result #{o}/#{r} exit:#{result[image_name][:status_code]}"
       end
-    elsif response.errors.any?
-       logger.error response.errors.messages["data"]
+
+      next if result.empty? || result.all? {|_,v| v[:status_code] == 0 }
+
+      t = ["# These image has vulnability."]
+      result.each do |k,v|
+        t << v[:stdout]
+      end
+
+      issue_txt = t.join("\n")
+        .gsub(/<head>.+?<\/head>/m, '')
+        .gsub(/<\/?body>/m, '')
+        .gsub(/<\/?html>/m, '')
+        .gsub(/^\d{4}-\d{2}-\d{2}T\d{2}.+$/, '')
+        .gsub(/<!DOCTYPE html>/, '')
+        .gsub(/^\s*$/, '')
+        .gsub(/^\n/, '')
+        .gsub(/^    /m, '')
+
+      logger.info "create issue #{o}/#{r}"
+      client.create_issue("#{o}/#{r}", "#{Date.today.strftime("%Y/%m/%d")} Found vulnerabilities in docker image", issue_txt)
+    rescue => e
+      logger.error "#{o}/#{r} happend error #{e}"
     end
+  elsif response.errors.any?
+    if response.errors.messages["data"].first =~ /API rate limit exceeded for user ID/
+      puts "redu"
+      sleep  60
+      redo
+    end
+    logger.error response.errors.messages["data"]
   end
 end
